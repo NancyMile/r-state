1b516551d6689f167f81a6252e99ff27687ac3e3
nancy <smithnancy@outlook.com>
2023-07-06 14:24:35 +1000

    app template  some buttons added to navigate, to start session and  home

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,42 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <v-card
+    elevation="3"
+    max-width="1200"
+    class="mx-auto"
+  >
+    <v-layout>
+        <v-app-bar
+          color="blue-darken-1"
+        >
+          <template v-slot:prepend>
+            <v-btn
+              :to="{name: 'home'}"
+            >
+              R-State
+            </v-btn>
+          </template>
+          <template v-slot:append>
+            <v-btn
+              :to="{name: 'home'}"
+            >
+              Home
+            </v-btn>
+            <v-btn
+              :to="{name: 'login'}"
+            >
+              Start Session
+            </v-btn>
+          </template>
+        </v-app-bar>
+
+        <v-main>
+          <v-container>
+            <RouterView/>
+          </v-container>
+        </v-main>
+
+    </v-layout>
+  </v-card>
 </template>
