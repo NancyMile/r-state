e2883c3aaed1c44b93a542f8851e03637f92a4d7
nancy <smithnancy@outlook.com>
2023-07-06 14:46:01 +1000

    login form added

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,5 +2,39 @@
 </script>
 
 <template>
-     <h1 class="text-center text-h3 font-weight-bold my-5">Start Session</h1>
+    <v-card
+        flat
+        max-width="600"
+        class="mx-auto my-10"
+    >
+        <v-card-title
+            class="text-h4 font-weight-bold"
+            tag="h3"
+        >
+            Start Session
+        </v-card-title>
+        <v-card-subtitle>
+            Start with your account.
+        </v-card-subtitle>
+
+        <v-form class="mt-5">
+            <v-text-field
+            type="email"
+            label="Email"
+            bg-color="blue-grey-lighten-5"
+            />
+            <v-text-field
+            type="password"
+            label="Password"
+            bg-color="blue-grey-lighten-5"
+            />
+            <v-btn
+                block
+                color="pink-accent-3"
+            >
+                Start Session
+            </v-btn>
+        </v-form>
+
+    </v-card>
 </template>
