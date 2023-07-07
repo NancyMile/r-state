1811a35c3feadc3aad010f756254ba316f1f537b
nancy <smithnancy@outlook.com>
2023-07-07 10:10:42 +1000

    view admin add propriety button added, and form created on add new propriety view

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -2,4 +2,11 @@
 </script>
 <template>
     <h2 class="text-center text-h3 my-5 font-weight-bold"> Admin Propriety</h2>
+    <v-btn
+        color="blue"
+        variant="flat"
+        :to="{name: 'new-propriety'}"
+    >
+        New propriety
+    </v-btn>
 </template>
--- src/views/admin/NewProprietyView.vue
+++ src/views/admin/NewProprietyView.vue
@@ -1,5 +1,79 @@
 <script setup>
+const items = [1, 2, 3, 4, 5]
 </script>
 <template>
-    <h2 class="text-center text-h3 my-5 font-weight-bold"> New Propriety</h2>
+    <v-card
+        max-width="800"
+        flat
+        class="mx-auto my-10"
+    >
+        <v-card-title
+            class="text-h4 font-weight-bold"
+            tag="h3"
+        >
+            New Propriety
+        </v-card-title>
+        <v-card-subtitle>
+            Please fill the from
+        </v-card-subtitle>
+
+        <v-form class="mt-10">
+            <v-text-field
+                class="mb-5"
+                label="Title Proprierty"
+            />
+            <v-file-input
+                accept="image/jpeg"
+                label ="Photo"
+                prepend-icon="mdi-camera"
+                class="mb-5"
+            />
+            <v-text-field
+                class="mb-5"
+                label="Price"
+            />
+            <v-row>
+                <v-col
+                    cols="12"
+                    md="4"
+                >
+                    <v-select
+                        label="rooms"
+                        class="mb-5"
+                        :items="items"
+                    />
+                </v-col>
+                <v-col
+                    cols="12"
+                    md="4"
+                >
+                    <v-select
+                        label="bathrooms"
+                        class="mb-5"
+                        :items="items"
+                    />
+                </v-col>
+               <v-col
+                    cols="12"
+                    md="4"
+               >
+                <v-select
+                    label="garages"
+                    class="mb-5"
+                    :items="items"
+                />
+               </v-col>
+            </v-row>
+            <v-textarea class="mb-5" label="Description"></v-textarea>
+            <v-checkbox
+                label=" balcony"
+            />
+            <v-btn
+                color="pink-accent-3"
+                block
+            >
+                Add
+            </v-btn>
+        </v-form>
+    </v-card>
 </template>
